4eb5703a759876053396dc4ace667ab0f05f034d
bigmogician <bigmogician@outlook.com>
2019-11-10 14:48:59 +0800

    feat: add antd ui library

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,18 @@
+import LOCALES_EN from "@angular/common/locales/en";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { registerLocaleData } from "@angular/common";
+import { NgZorroAntdModule, NZ_I18N, en_US } from "ng-zorro-antd";
 import { ErrorsModule } from "../pages/errors/errors.module";
 import { AppRoutingModule } from "../shared/router.module";
 import { AppComponent } from "./app.component";
 
+registerLocaleData(LOCALES_EN);
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ErrorsModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, NgZorroAntdModule, ErrorsModule, AppRoutingModule],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
