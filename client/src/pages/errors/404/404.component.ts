ab0fdc4dc0a4d0455b88eb076a2dba660c46ad0f
bigmogician <bigmogician@outlook.com>
2019-11-10 14:21:45 +0800

    fix: errors use scss

--- client/src/pages/errors/404/404.component.ts
+++ client/src/pages/errors/404/404.component.ts
@@ -3,5 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-error-404",
   templateUrl: "./404.html",
+  styleUrls: ["../styles/index.scss"],
 })
 export class Error404Component {}
--- client/src/pages/errors/500/500.component.ts
+++ client/src/pages/errors/500/500.component.ts
@@ -3,5 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-error-500",
   templateUrl: "./500.html",
+  styleUrls: ["../styles/index.scss"],
 })
 export class Error500Component {}
