78e58a0eb2ac97fa7e19ea75eefcb6ed0efff8b5
Big Mogician <sunaoran@youzan.com>
2019-11-29 17:53:22 +0800

    fix: no chokidar need

--- server/src/app/bootstrap.dev.ts
+++ server/src/app/bootstrap.dev.ts
@@ -1,16 +1,18 @@
-import * as chokidar from "chokidar";
-import * as path from "path";
+// import * as chokidar from "chokidar";
+// import * as path from "path";
 import { bootstrap as base } from "./bootstrap.prod";
 import { IConfigs } from "../configs/config";
 
-const serverRoot = path.resolve(__dirname, "..", "..", "src");
-const assetsRoot = path.resolve(serverRoot, "assets", "**", "*");
-const appRoot = path.resolve(serverRoot, "app", "**", "*");
-const configsRoot = path.resolve(serverRoot, "configs", "**", "*");
+// const serverRoot = path.resolve(__dirname, "..", "..", "src");
+// const assetsRoot = path.resolve(serverRoot, "assets", "**", "*");
+// const appRoot = path.resolve(serverRoot, "app", "**", "*");
+// const configsRoot = path.resolve(serverRoot, "configs", "**", "*");
+// const controllersRoot = path.resolve(serverRoot, "controllers", "**", "*");
+// const servicesRoot = path.resolve(serverRoot, "services", "**", "*");
 
 export async function bootstrap(configs: IConfigs) {
-  chokidar.watch([assetsRoot, appRoot, configsRoot]).on("change", pathname => {
-    console.log("file changed --> " + pathname);
-  });
+  // chokidar.watch([assetsRoot, appRoot, configsRoot, controllersRoot, servicesRoot]).on("change", pathname => {
+  //   console.log("file changed --> " + pathname);
+  // });
   return base(configs);
 }
