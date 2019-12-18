285bb17a7ddc2435b1780010fbfb80b2910af058
Big Mogician <sunaoran@youzan.com>
2019-12-18 13:01:42 +0800

    fix: export auth service

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -4,13 +4,12 @@ import { SiteController } from "./controllers/site.controller";
 import { PortalController } from "./controllers/portal.controller";
 import { ConfigService } from "./services/config.service";
 import { CompileService } from "./services/compile.service";
-import { RolesGuard } from "./guards/roles.guard";
-import { AuthService, FakeAuthService } from "./services/auth.service";
 
 @Global()
 @Module({
   imports: [],
   controllers: [ApiController, SiteController, PortalController],
-  providers: [ConfigService, CompileService, RolesGuard, { provide: AuthService, useClass: FakeAuthService }],
+  providers: [ConfigService, CompileService],
+  exports: [ConfigService, CompileService],
 })
 export class AppModule {}
--- server/src/main.module.ts
+++ server/src/main.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppModule } from "./app/app.module";
+import { AuthService, FakeAuthService } from "./app/services/auth.service";
 
 @Module({
   imports: [AppModule],
   controllers: [],
-  providers: [],
+  providers: [{ provide: AuthService, useClass: FakeAuthService }],
 })
 export class MainModule {}
