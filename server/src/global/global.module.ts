ecd893ca1e27ad11e10caba9060827560336dd8d
Big Mogician <sunaoran@youzan.com>
2019-12-18 13:46:45 +0800

    fix: make global catch all modules

--- server/src/global/global.module.ts
+++ server/src/global/global.module.ts
@@ -1,12 +1,13 @@
 import { Module, Global } from "@nestjs/common";
 import { AuthService } from "@global/services/auth.service";
 import { FakeAuthService } from "@app/services/auth.service";
+import { AppModule } from "@app/app.module";
 
 @Global()
 @Module({
-  imports: [],
+  imports: [AppModule],
   controllers: [],
   providers: [{ provide: AuthService, useClass: FakeAuthService }],
-  exports: [AuthService],
+  exports: [AppModule, AuthService],
 })
 export class GlobalModule {}
