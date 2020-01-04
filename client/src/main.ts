3350dbbecdf25ca8dcc8d86b62285e443ef6488b
root <root@DESKTOP-BigMogician.localdomain>
2020-01-04 15:40:01 +0800

    fix: use websdk

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,11 @@
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { Factory } from "@amoebajs/builder-websdk";
 
 import { AppModule } from "./app/app.module";
 import { ENV } from "./env";
+
+console.log(Factory);
 
 if (ENV.production) {
   enableProdMode();
