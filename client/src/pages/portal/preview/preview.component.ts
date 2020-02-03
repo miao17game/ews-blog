76c28ad299a42b9b3781142a45709c965f21d969
root <root@DESKTOP-BigMogician.localdomain>
2020-02-03 17:58:52 +0800

    feat: better preview client page

--- client/src/pages/portal/preview/preview.component.ts
+++ client/src/pages/portal/preview/preview.component.ts
@@ -14,6 +14,8 @@ export class PortalPreviewComponent implements OnInit, AfterViewInit {
   @ViewChild("previewHost", { static: false }) previewHost: ElementRef;
 
   public showButton = false;
+  public showEditor = true;
+  public showPreview = false;
   public pageConfigs = createDefaultConfigs();
 
   private vm!: VM;
@@ -53,25 +55,34 @@ export class PortalPreviewComponent implements OnInit, AfterViewInit {
     this.showButton = true;
   }
 
-  async onTabChange(e: NzTabChangeEvent) {
-    if (e.index === 1) {
-      try {
-        const configs = JSON.parse(yamljs.safeLoad(this.pageConfigs));
-        const result = await this.portal.createSource("yaml", configs);
-        if (this.vm) {
-          this.vm.applyFsDiff({
-            create: {
-              "src/index.js": result.data.source,
-            },
-            destroy: [],
-          });
-        } else {
-          this.project.files["src/index.js"] = result.data.source;
-          this.onStart();
-        }
-      } catch (error) {
-        this.message.error(JSON.stringify(error.toString()));
+  onEditorClick() {
+    this.showEditor = !this.showEditor;
+  }
+
+  onPreviewClick() {
+    this.showPreview = !this.showPreview;
+    if (this.showPreview) {
+      this.runUpdate();
+    }
+  }
+
+  private async runUpdate() {
+    try {
+      const configs = yamljs.safeLoad(this.pageConfigs);
+      const result = await this.portal.createSource("json", configs);
+      if (this.vm) {
+        this.vm.applyFsDiff({
+          create: {
+            "src/index.js": result.data.source,
+          },
+          destroy: [],
+        });
+      } else {
+        this.project.files["src/index.js"] = result.data.source;
+        this.onStart();
       }
+    } catch (error) {
+      this.message.error(JSON.stringify(error.toString()));
     }
   }
 
@@ -81,7 +92,7 @@ export class PortalPreviewComponent implements OnInit, AfterViewInit {
       hideDevTools: true,
       hideNavigation: true,
       forceEmbedLayout: true,
-      height: "750px",
+      height: "760px",
       view: "preview",
     }).then(vm => {
       // TODO
@@ -89,182 +100,213 @@ export class PortalPreviewComponent implements OnInit, AfterViewInit {
       console.log(vm);
     });
   }
+
+  onTextareaChange(value: string) {
+    if (this.showPreview) {
+      this.runUpdate();
+    }
+  }
 }
 
 function createDefaultConfigs() {
-  return `{
-    "components": [
-      {
-        "module": "ambjs-common-component-module",
-        "name": "zent-button",
-        "id": "ZentBtnDemoComponent"
-      }
-    ],
-    "page": {
-      "module": "ambjs-common-component-module",
-      "name": "css-grid-container",
-      "id": "CssGridPageDemoRoot",
-      "children": [
-        {
-          "ref": "ZentBtnDemoComponent",
-          "id": "ZentBtnDemoCompRef01",
-          "props": {
-            "loading": {
-              "type": "state",
-              "expression": "zentBtnLoading"
-            },
-            "children": {
-              "type": "literal",
-              "syntaxType": "string",
-              "expression": "BUTTON01"
-            },
-            "size": {
-              "type": "literal",
-              "syntaxType": "string",
-              "expression": "large"
-            },
-            "type": {
-              "type": "state",
-              "expression": "zentBtnType"
-            }
-          }
-        },
-        {
-          "ref": "ZentBtnDemoComponent",
-          "id": "ZentBtnDemoCompRef02",
-          "props": {
-            "loading": {
-              "type": "literal",
-              "syntaxType": "boolean",
-              "expression": false
-            },
-            "children": {
-              "type": "state",
-              "expression": "zentBtn02Content"
-            },
-            "size": {
-              "type": "literal",
-              "syntaxType": "string",
-              "expression": "large"
-            },
-            "type": {
-              "type": "literal",
-              "syntaxType": "string",
-              "expression": "primary"
-            }
-          }
-        }
-      ],
-      "directives": [
-        {
-          "module": "ambjs-common-directive-module",
-          "name": "zent-base-css",
-          "id": "ZentBaseCssDemoDirective"
-        },
-        {
-          "module": "ambjs-common-directive-module",
-          "name": "custom-click",
-          "id": "CustomClickDemoDirective",
-          "input": {
-            "host": {
-              "type": "literal",
-              "expression": "ZentBtnDemoCompRef02"
-            },
-            "eventType": {
-              "type": "literal",
-              "expression": "setState"
-            },
-            "attrName": {
-              "type": "literal",
-              "expression": "onClick"
-            },
-            "targetName": {
-              "type": "literal",
-              "expression": "zentBtn02Content"
-            },
-            "expression": {
-              "type": "literal",
-              "expression": "e => new Date().getTime()"
-            }
-          }
-        }
-      ],
-      "input": {
-        "basic": {
-          "useComponentState": {
-            "type": "literal",
-            "expression": true
-          },
-          "defaultComponentState": {
-            "type": "literal",
-            "expression": {
-              "btn01Text": "10002",
-              "zentBtnLoading": true,
-              "zentBtnType": "danger",
-              "zentBtn02Content": "BUTTON02"
-            },
-            "objectState": {
-              "type": "literal",
-              "expression": {
-                "id": "xxx",
-                "name": "yyy"
-              }
-            },
-            "arrayState": {
-              "type": "literal",
-              "expression": [
-                {
-                  "id": "xxx",
-                  "name": "yyy"
-                },
-                {
-                  "id": "zzz",
-                  "name": "aaa"
-                }
-              ]
-            }
-          }
-        },
-        "gridTemplateColumnsCount": {
-          "type": "literal",
-          "expression": 2
-        },
-        "gridTemplateRowsFrs": {
-          "type": "literal",
-          "expression": [
-            1,
-            2
-          ]
-        }
-      },
-      "attach": {
-        "childRowStart": {
-          "type": "childRefs",
-          "expression": [
-            {
-              "id": "ZentBtnDemoCompRef01",
-              "value": 1
-            },
-            {
-              "id": "ZentBtnDemoCompRef02",
-              "value": 2
-            }
-          ]
-        },
-        "childColumnStart": {
-          "type": "childRefs",
-          "expression": [
-            {
-              "id": "ZentBtnDemoCompRef01",
-              "value": 1
-            },
-            {
-              "id": "ZentBtnDemoCompRef02",
-              "value": 2
-            }
-          ]
-        }
-      }
-    }
-  }`;
+  return `provider: react
+framework:
+  sdk: "0.0.1"
+components:
+  - id: GridLayout
+    name: grid-layout
+    module: ambjs-layout-module
+    version: 0.0.1-beta.0
+  - id: StackLayout
+    name: stack-layout
+    module: ambjs-layout-module
+    version: 0.0.1-beta.0
+  - id: ZentButton
+    name: zent-button
+    module: zent-module
+    version: "0.0.1"
+directives:
+  - id: GlobalState
+    name: global-state
+    module: ambjs-common-module
+    version: "0.0.1-beta.0"
+  - id: ZentCssImport
+    name: zent-base-css
+    module: zent-module
+    version: "0.0.1"
+page:
+  ref: GridLayout
+  id: GridLayoutPageRoot
+  slot: app
+  input:
+    width:
+      type: literal
+      expression: 100vw
+    height:
+      type: literal
+      expression: 100vh
+    rowCount:
+      type: literal
+      expression: 2
+    columnCount:
+      type: literal
+      expression: 3
+    rowSizes:
+      type: literal
+      expression:
+        - [1, 50]
+        - [2, 50]
+    columnSizes:
+      type: literal
+      expression:
+        - [1, 30]
+        - [2, 40]
+        - [3, 30]
+  directives:
+    - ref: ZentCssImport
+      id: ZentCssImportInstance01
+    - ref: GlobalState
+      id: GlobalStateInstance01
+      input:
+        name:
+          type: literal
+          expression: AppContext
+        state:
+          type: literal
+          expression:
+            - [demoNumber, 123456]
+            - [buttonName, XXXXXXXXXX]
+            - [objectState, { "a": 24523, "b": false }]
+  children:
+    - ref: GridLayout
+      id: GridLayoutChild01
+      input:
+        backgroundColor:
+          type: literal
+          expression: "#fea500"
+        padding:
+          type: literal
+          expression:
+            - [top, 10px]
+            - [left, 10px]
+            - [right, 10px]
+            - [bottom, 10px]
+      children:
+        - ref: StackLayout
+          id: StackLayoutChild01
+          input:
+            backgroundColor:
+              type: literal
+              expression: "#888888"
+          children:
+            - ref: ZentButton
+              id: ZentButtonInstance02
+              props:
+                children:
+                  type: literal
+                  syntaxType: string
+                  expression: BUTTON01
+            - ref: ZentButton
+              id: ZentButtonInstance03
+              props:
+                children:
+                  type: literal
+                  syntaxType: string
+                  expression: BUTTON02
+                type:
+                  type: literal
+                  syntaxType: string
+                  expression: primary
+            - ref: ZentButton
+              id: ZentButtonInstance04
+              props:
+                children:
+                  type: literal
+                  syntaxType: string
+                  expression: BUTTON03
+                type:
+                  type: literal
+                  syntaxType: string
+                  expression: danger
+    - ref: GridLayout
+      id: GridLayoutChild02
+      input:
+        backgroundColor:
+          type: literal
+          expression: "#323233"
+    - ref: GridLayout
+      id: GridLayoutChild03
+      input:
+        backgroundColor:
+          type: literal
+          expression: "rgb(254, 38, 76)"
+    - ref: GridLayout
+      id: GridLayoutChild04
+      input:
+        backgroundColor:
+          type: literal
+          expression: "rgb(54, 158, 106)"
+      children:
+        - ref: ZentButton
+          id: ZentButtonInstance01
+          props:
+            loading:
+              type: state
+              expression: objectState.b
+            children:
+              type: state
+              expression: buttonName
+            size:
+              type: literal
+              syntaxType: string
+              expression: large
+            type:
+              type: literal
+              syntaxType: string
+              expression: danger
+  attach:
+    rowStart:
+      type: childRefs
+      expression:
+        - id: GridLayoutChild01
+          value: 1
+        - id: GridLayoutChild02
+          value: 1
+        - id: GridLayoutChild03
+          value: 2
+        - id: GridLayoutChild04
+          value: 2
+    columnStart:
+      type: childRefs
+      expression:
+        - id: GridLayoutChild01
+          value: 1
+        - id: GridLayoutChild02
+          value: 2
+        - id: GridLayoutChild03
+          value: 1
+        - id: GridLayoutChild04
+          value: 3
+    rowSpan:
+      type: childRefs
+      expression:
+        - id: GridLayoutChild01
+          value: 1
+        - id: GridLayoutChild02
+          value: 1
+        - id: GridLayoutChild03
+          value: 1
+        - id: GridLayoutChild04
+          value: 1
+    columnSpan:
+      type: childRefs
+      expression:
+        - id: GridLayoutChild01
+          value: 1
+        - id: GridLayoutChild02
+          value: 2
+        - id: GridLayoutChild03
+          value: 2
+        - id: GridLayoutChild04
+          value: 1`;
 }
