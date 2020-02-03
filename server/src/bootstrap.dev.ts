23a00313b89b0108094da4728790fe193f808d27
root <root@DESKTOP-BigMogician.localdomain>
2020-02-03 16:44:01 +0800

    fix: dev cors fix

--- server/src/bootstrap.dev.ts
+++ server/src/bootstrap.dev.ts
@@ -6,7 +6,10 @@ export async function bootstrap(options: Partial<IBootstrapOptions> = {}) {
     staticOptions: { maxAge: 0 },
     beforeListen: app => {
       app.engine("html", useNunjucks(app, { noCache: true }).render);
-      app.enableCors({ origin: "http://localhost:4200", credentials: true });
+      app.enableCors({
+        origin: [/http:localhost:[0-9]{1,5}/, /http:\/\/127\.0\.0\.1:[0-9]{1,5}/],
+        credentials: true,
+      });
     },
   });
 }
