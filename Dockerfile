9db942783be8a81b6bd6f54a9d15d7684efe35ac
bigmogician <sunaoran@youzan.com>
2020-02-08 01:44:33 +0800

    fix: quick docker without client bigsize

--- Dockerfile
+++ Dockerfile
@@ -10,10 +10,6 @@ ADD package.json /app/package.json
 ADD yarn.lock /app/yarn.lock
 RUN cd /app && yarn install
 
-ADD client/package.json /app/client/package.json
-ADD client/yarn.lock /app/client/yarn.lock
-RUN cd /app/client && yarn install
-
 ADD server/package.json /app/server/package.json
 ADD server/yarn.lock /app/server/yarn.lock
 RUN cd /app/server && yarn install
@@ -21,13 +17,7 @@ RUN cd /app/server && yarn install
 ADD server /app/server
 RUN cd /app/server && yarn build
 
-ADD webpack.config.js /app/webpack.config.js
-ADD tsconfig.websdk.json /app/tsconfig.websdk.json
-ADD scripts/build-sdk.js /app/scripts/build-sdk.js
-RUN cd /app && yarn build:websdk
-
-ADD client /app/client
-RUN cd /app/client && yarn build
+ADD client/dist /app/client/dist
 
 WORKDIR /app
 
--- scripts/docker.js
+++ scripts/docker.js
@@ -1,13 +1,35 @@
 const path = require("path");
-const { spawn } = require("child_process");
+const { spawn, spawnSync } = require("child_process");
 
 const args = process.argv.slice(2);
 
+const yarn = /^win/.test(process.platform) ? "yarn.cmd" : "yarn";
+
 let version = `0.0.${new Date().getTime()}`;
+let quick = false;
 for (const argv of args) {
   if (argv.startsWith("-v=") || argv.startsWith("--version=")) {
     version = argv.split("=")[1];
+    continue;
   }
+  if (argv.startsWith("-q=") || argv.startsWith("--quick=")) {
+    quick = argv.split("=")[1] === "true";
+    continue;
+  }
+}
+
+function runSync(command, args) {
+  return spawnSync(command, args, {
+    env: process.env,
+    cwd: path.resolve(__dirname, ".."),
+    encoding: "utf8",
+    stdio: ["pipe", process.stdout, process.stderr],
+  });
+}
+
+if (!quick) {
+  runSync(yarn, ["install:all"]);
+  runSync(yarn, ["build:client"]);
 }
 
 const command = "docker";
